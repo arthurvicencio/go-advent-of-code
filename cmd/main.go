e97e83caab9eabc3ffb044d43b6f86cecfa10bde
arthurvicencio <artsvicencio@gmail.com>
2024-12-02 01:14:23 +0800

    fix: check args

--- cmd/main.go
+++ cmd/main.go
@@ -13,13 +13,20 @@ func main() {
 	problems := make(map[string]aocutils.Problem)
 	mergeMap(problems, twenty_twenty_four.Problems())
 
-	if _, exists := problems[os.Args[1]]; !exists {
-		fmt.Printf("no solution for \"%s\"\n", os.Args[1])
+	if len(os.Args) < 2 {
+		fmt.Println("missing name argument")
 		os.Exit(1)
 		return
 	}
 
-	problems[os.Args[1]].Solve()
+	name := os.Args[1]
+	if _, exists := problems[name]; !exists {
+		fmt.Printf("no solution for \"%s\"\n", name)
+		os.Exit(1)
+		return
+	}
+
+	problems[name].Solve()
 }
 
 func mergeMap[A, B comparable](a, b map[A]B) {
