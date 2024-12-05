89a9f7ce5964248e94afc54efffe22d96e146771
arthurvicencio <artsvicencio@gmail.com>
2024-12-06 00:47:57 +0800

    day-05

---
+++ pkg/2024/day-05/part-2.go
@@ -0,0 +1,81 @@
+package twenty_twenty_four_day_05
+
+import (
+	_ "embed"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input2 string
+
+type Part2 struct{}
+
+func (p Part2) Solve() {
+	rules := make(map[int]map[int]bool)
+	updates := make([][]int, 0)
+
+	rawInput := strings.Split(input2, "\n\n")
+
+	for _, line := range strings.Split(rawInput[0], "\n") {
+		var a, b int
+		fmt.Sscanf(line, "%d|%d", &a, &b)
+		if _, ok := rules[b]; !ok {
+			rules[b] = make(map[int]bool)
+		}
+		rules[b][a] = true
+	}
+
+	for _, line := range strings.Split(rawInput[1], "\n") {
+		update := make([]int, 0)
+		for _, char := range strings.Split(line, ",") {
+			n, _ := strconv.Atoi(char)
+			update = append(update, n)
+		}
+		updates = append(updates, update)
+	}
+
+	var answer int
+	for _, update := range updates {
+		sorted := p.sort(rules, update)
+		if !p.isSorted(sorted, update) {
+			answer += sorted[len(update)/2]
+		}
+	}
+	fmt.Println(answer)
+}
+
+func (p Part2) sort(rules map[int]map[int]bool, slice []int) []int {
+	numPriorities := make(map[int]int)
+	for _, num := range slice {
+		numPriorities[num] = 0
+	}
+
+	for _, num := range slice {
+		for numAfter, numsBefore := range rules {
+			if _, ok := numsBefore[num]; ok {
+				numPriorities[numAfter]++
+			}
+		}
+	}
+
+	sorted := make([]int, len(slice))
+	copy(sorted, slice)
+
+	sort.Slice(sorted, func(i, j int) bool {
+		return numPriorities[sorted[i]] < numPriorities[sorted[j]]
+	})
+
+	return sorted
+}
+
+func (p Part2) isSorted(a, b []int) bool {
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
--- pkg/2024/problems.go
+++ pkg/2024/problems.go
@@ -3,15 +3,19 @@ package twenty_twenty_four
 import (
 	twenty_twenty_four_day_01 "github.com/arthurvicencio/go-advent-of-code/pkg/2024/day-01"
 	twenty_twenty_four_day_02 "github.com/arthurvicencio/go-advent-of-code/pkg/2024/day-02"
+	twenty_twenty_four_day_05 "github.com/arthurvicencio/go-advent-of-code/pkg/2024/day-05"
 	"github.com/arthurvicencio/go-advent-of-code/pkg/aocutils"
 )
 
 func Problems() map[string]aocutils.Problem {
 	return map[string]aocutils.Problem{
-		"2024-01-1": twenty_twenty_four_day_01.Part1{},
-		"2024-01-2": twenty_twenty_four_day_01.Part2{},
+		"2024-d01-p1": twenty_twenty_four_day_01.Part1{},
+		"2024-d01-p2": twenty_twenty_four_day_01.Part2{},
 
-		"2024-02-1": twenty_twenty_four_day_02.Part1{},
-		"2024-02-2": twenty_twenty_four_day_02.Part2{},
+		"2024-d02-p1": twenty_twenty_four_day_02.Part1{},
+		"2024-d02-p2": twenty_twenty_four_day_02.Part2{},
+
+		"2024-d05-p1": twenty_twenty_four_day_05.Part1{},
+		"2024-d05-p2": twenty_twenty_four_day_05.Part2{},
 	}
 }
